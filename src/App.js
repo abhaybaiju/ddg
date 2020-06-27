6b2f821d80e98aae9684e91df12cfd9413aa96d6
abhay.baiju <abhay0198@gmail.com>
2020-06-27 15:09:51 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
+import illustration1 from './illustration1.svg';
+import { Typography,  Slider, Paper } from '@material-ui/core';
 import './App.css';
 
 function App() {
+ 
+
+  const marks = [
+    { value: 0, label: "😔" },
+    { value: 10, label: 1 },
+    { value: 20, label: 2 },
+    { value: 30, label: 3 },
+    { value: 40, label: 4 },
+    { value: 50, label: "😐" },
+    { value: 60, label: 6 },
+    { value: 70, label: 7 },
+    { value: 80, label: 8 },
+    { value: 90, label: 9 },
+    { value: 100, label: "😊" }
+  ];
+
+
+  const scaler = (num) => {
+    return num/10;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+     <Typography style= {{paddingTop :60, paddingBottom:50}} variant="h1">
+       How happy are you ?
+     </Typography>
+     <img src={illustration1} alt="svg" style= {{paddingTop :0, paddingBottom:50, width:450}}></img>
+     <Slider
+      style={{width: "60%" , marginTop:20, paddingBottom:50}}
+      defaultValue={50}
+      step={5}
+      marks={marks}
+      valueLabelDisplay="auto"
+      scale={scaler}
+/>
+     <Typography style= {{paddingTop :20, paddingBottom: 40}} variant="subtitle1">
+       See how you compare to the world 
+     </Typography>
+
+    <div className="paperbox">
+     <Paper  style= {{width:160, height:160, margin:20 }}elevation={4} >
+     <Typography style= {{padding :10, paddingTop:60}} variant="subtitle1">
+       Sample Text 
+     </Typography>
+     </Paper>
+     <Paper  style= {{width:160, height:160, margin:20}} elevation={4} >
+     <Typography style= {{padding :10, paddingTop:60}} variant="subtitle1">
+       Sample Text 
+     </Typography>
+     </Paper>
+     <Paper  style= {{width:160, height:160, margin:20}} elevation={4} >
+     <Typography style= {{padding :10, paddingTop:60}} variant="subtitle1">
+       Sample Text 
+     </Typography>
+     </Paper>
+     <Paper  style= {{width:160, height:160, margin:20}} elevation={4} >
+     <Typography style= {{padding :10, paddingTop:60}} variant="subtitle1">
+       Sample Text 
+     </Typography>
+     </Paper>
+     <Paper  style= {{width:160, height:160, margin:20}} elevation={4} >
+     <Typography style= {{padding :10, paddingTop:60}} variant="subtitle1">
+       Sample Text 
+     </Typography>
+     </Paper>
+     <Paper  style= {{width:160, height:160, margin:20}} elevation={4} >
+     <Typography style= {{padding :10, paddingTop:60}} variant="subtitle1">
+       Sample Text 
+     </Typography>
+     </Paper>
+     
+     </div>
     </div>
   );
 }
