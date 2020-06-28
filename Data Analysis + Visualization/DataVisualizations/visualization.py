0d7dc314464a7439558b5a973264bc6cd4898d0b
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2020-06-27 22:23:22 -0400

    Tweaked growth size of data points and added used SciKitLearn for regression on one of the scatterplots.

--- Data Analysis + Visualization/DataVisualizations/visualization.py
+++ Data Analysis + Visualization/DataVisualizations/visualization.py
@@ -1,10 +1,17 @@
 import pandas as pd
+
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.offline import plot
+from plotly.subplots import make_subplots
 
-world_happiness_path = "datasets/world-happiness/"
-gapminder_path = "datasets/gapminder/datasets_5533_8279_gapminder.tsv"
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing, utils
+encoder = preprocessing.LabelEncoder()
+
+world_happiness_path = "../datasets/world-happiness/"
+gapminder_path = "../datasets/gapminder/datasets_5533_8279_gapminder.tsv"
 
 data2015_wh = pd.read_csv(world_happiness_path + "2015.csv")
 data2016_wh = pd.read_csv(world_happiness_path + "2016.csv")
@@ -30,7 +37,7 @@ l_data2015_wh = dict(title='Happiness Index 2015'
 choropleth2015_wh = go.Figure(data=[d_data2015_wh]
                               , layout=l_data2015_wh)
 choropleth2015_wh.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-plot(choropleth2015_wh, filename='DataVisualizations/choropleth2015.html', auto_open=False)
+plot(choropleth2015_wh, filename='choropleth2015.html', auto_open=False)
 
 # data2016_wh - choropleth
 d_data2016_wh = dict(type='choropleth'
@@ -45,7 +52,7 @@ l_data2016_wh = dict(title='Happiness Index 2016'
 choropleth2016_wh = go.Figure(data=[d_data2016_wh]
                               , layout=l_data2016_wh)
 choropleth2016_wh.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-plot(choropleth2016_wh, filename='DataVisualizations/choropleth2016.html', auto_open=False)
+plot(choropleth2016_wh, filename='choropleth2016.html', auto_open=False)
 
 # data2017_wh - choropleth
 d_data2017_wh = dict(type='choropleth'
@@ -60,7 +67,7 @@ l_data2017_wh = dict(title='Happiness Index 2017'
 choropleth2017_wh = go.Figure(data=[d_data2017_wh]
                               , layout=l_data2017_wh)
 choropleth2017_wh.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-plot(choropleth2017_wh, filename='DataVisualizations/choropleth2017.html', auto_open=False)
+plot(choropleth2017_wh, filename='choropleth2017.html', auto_open=False)
 
 # data2018_wh - choropleth
 d_data2018_wh = dict(type='choropleth'
@@ -75,7 +82,7 @@ l_data2018_wh = dict(title='Happiness Index 2018'
 choropleth2018_wh = go.Figure(data=[d_data2018_wh]
                               , layout=l_data2018_wh)
 choropleth2018_wh.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-plot(choropleth2018_wh, filename='DataVisualizations/choropleth2018.html', auto_open=False)
+plot(choropleth2018_wh, filename='choropleth2018.html', auto_open=False)
 
 # data2019_wh - choropleth
 d_data2019_wh = dict(type='choropleth'
@@ -90,7 +97,7 @@ l_data2019_wh = dict(title='Happiness Index 2019'
 choropleth2019_wh = go.Figure(data=[d_data2019_wh]
                               , layout=l_data2019_wh)
 choropleth2019_wh.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-plot(choropleth2019_wh, filename='DataVisualizations/choropleth2019.html', auto_open=False)
+plot(choropleth2019_wh, filename='choropleth2019.html', auto_open=False)
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """Relationship Between GDP, Life Expectancy, and Perceptions of Corruption"""''
@@ -106,7 +113,7 @@ gdp_life_corruption = go.Figure(data=[go.Scatter3d(
     , mode='markers'
     , text=data2019_wh['Country or region']
     , marker=dict(
-        size=6
+        size=data2019_wh['Perceptions of corruption'] * data2019_wh['Healthy life expectancy'] * data2019_wh['GDP per capita'] * 50 + 10
         , color=z_data_c
         , colorscale='Viridis'
         , opacity=0.8
@@ -122,7 +129,7 @@ gdp_life_corruption.update_layout(scene=dict(
     , margin=dict(l=0, r=0, b=0, t=0)
 )
 
-plot(gdp_life_corruption, filename ='DataVisualizations/gdp_lifeExp_corruption.html', auto_open=False)
+plot(gdp_life_corruption, filename ='gdp_lifeExp_corruption.html', auto_open=False)
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """"""""""Relationship Between GDP, Life Expectancy, and Freedom'"""""""""""""""
@@ -138,7 +145,7 @@ gdp_life_freedom = go.Figure(data=[go.Scatter3d(
     , mode='markers'
     , text=data2019_wh['Country or region']
     , marker=dict(
-        size=6
+        size=data2019_wh['Freedom to make life choices'] * data2019_wh['Healthy life expectancy'] * data2019_wh['GDP per capita'] * 40 + 10
         , color=z_data_f
         , colorscale='Viridis'
         , opacity=0.8
@@ -154,7 +161,7 @@ gdp_life_freedom.update_layout(scene=dict(
     , margin=dict(l=0, r=0, b=0, t=0)
 )
 
-plot(gdp_life_freedom, filename ='DataVisualizations/gdp_lifeExp_freedom.html', auto_open=False)
+plot(gdp_life_freedom, filename ='gdp_lifeExp_freedom.html', auto_open=False)
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """""""""""""""Relationship Between GDP and Life Expectancy"""""""""""""""""""""
@@ -169,6 +176,7 @@ gdp_lifeExp = px.scatter(
     , size="pop"
     , color="continent"
     , hover_name="country"
+    , trendline="ols"
     , log_x=True
     , size_max=55
     , range_x=[100, 100000]
@@ -182,12 +190,33 @@ gdp_lifeExp.update_layout(
     , margin=dict(l=0, r=0, b=0, t=40)
 )
 
-plot(gdp_lifeExp, filename='DataVisualizations/gdp_lifeExp.html', auto_open=False)
+plot(gdp_lifeExp, filename='gdp_lifeExp.html', auto_open=False)
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """""""""""""""Relationship Between GDP and Happiness Index"""""""""""""""""""""
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-gdp_happiness = go.Figure(data=go.Scatter(
+# Linear Regression for predictions
+pred_x = []
+pred_y = []
+
+
+X_gdp_happiness = data2019_wh['GDP per capita'].values
+y_gdp_happiness = data2019_wh['Score'].values
+X_gdp_happiness = X_gdp_happiness.reshape(-1, 1)
+
+model = LinearRegression()
+model.fit(X_gdp_happiness, y_gdp_happiness)
+
+for x in range(220):
+    temp = [x]
+    prediction = model.predict([[x/100]])
+    pred_x.append(x/100)
+    pred_y.append(prediction[0])
+
+print(pred_y)
+
+# plot
+gdp_happiness_plt = go.Scatter(
     x=data2019_wh['GDP per capita']
     , y=data2019_wh['Score']
     , mode='markers'
@@ -196,13 +225,24 @@ gdp_happiness = go.Figure(data=go.Scatter(
         size=data2019_wh['Score'] ** 1.8
         , color=data2019_wh['Score']
     )
-))
+)
 
-gdp_happiness.update_layout(
+gdp_happiness_lbf = go.Scatter(
+    x=pred_x
+    , y=pred_y
+    , name="Predictions"
+)
+
+gdp_happiness_data = [gdp_happiness_plt, gdp_happiness_lbf]
+gdp_happiness_layout = go.Layout()
+
+gdp_happiness_fig = go.Figure(data=gdp_happiness_data, layout=gdp_happiness_layout)
+
+gdp_happiness_fig.update_layout(
     xaxis_title='GDP Per Capita ($100,000s)'
     , yaxis_title='Happiness Score'
     , title="GDP Vs Happiness"
     , margin=dict(l=0, r=0, b=0, t=40)
 )
 
-plot(gdp_happiness, filename='DataVisualizations/gdp_happiness.html', auto_open=False)
+plot(gdp_happiness_fig, filename='gdp_happiness.html', auto_open=False)
